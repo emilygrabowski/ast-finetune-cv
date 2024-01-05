f5b2641a132deec87bd8be9e161a4a6d71383b3e
emilygrabowski <emilygrabowski@users.noreply.github.com>
2024-01-05 10:44:24 -0800

    Update load-dataset.py
    Add code to check whether the path exists before trying to run load_dataset

--- load-dataset.py
+++ load-dataset.py
@@ -1,3 +1,10 @@
 from datasets import load_dataset
 
 import os
+
+
+dir_name = "../data/150-percent"
+
+print("Checking that the path exists...:",os.path.exists(dir_name))
+
+dataset=load_dataset(dir_name)
